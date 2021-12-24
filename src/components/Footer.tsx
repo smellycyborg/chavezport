e4c8896a654338e10100acb9a14c93b79c30bcf5
chavez <95760172+osum-posum@users.noreply.github.com>
2021-12-24 17:46:17 -0500

    bruh

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,16 +7,7 @@ interface Props {
     link2: string,
 }
 
-const click1 = (
-    event: React.MouseEvent<Element, MouseEvent>, 
-    link: string, 
-    text: string
-    ) => {
-    window.open(link)
-    console.log(text)
-}
-
-const click2 = (
+const click = (
     event: React.MouseEvent<Element, MouseEvent>, 
     link: string, 
     text: string
@@ -33,12 +24,12 @@ export const Footer: React.FC<Props> = ( { link1, link2 } ) => {
                 <Icon className='icon' 
                 icon="ph:github-logo-thin" height="39" 
                 onClick={(e: React.MouseEvent<Element, MouseEvent>) => {
-                    click1(e, link1, 'clicked github')
+                    click(e, link1, 'clicked github')
                 }}/>
                 <Icon className='icon' 
                 icon="ph:linkedin-logo-thin" height="42" 
                 onClick={(e: React.MouseEvent<Element, MouseEvent>) => {
-                    click2(e, link2, 'clicked linkedin')
+                    click(e, link2, 'clicked linkedin')
                 }}/>
             </div>
             <p>site by chavez</p>
