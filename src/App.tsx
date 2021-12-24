8f4552aba50d1a114eeac4f9ffc323c0f792a149
chavez <95760172+osum-posum@users.noreply.github.com>
2021-12-24 16:21:05 -0500

    bruh

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const App: React.FC = () => {
 
   return <>
   <div>
-    <Header name='chavez.tech' bio='software engineer'/>
+    <Header name='chavez' bio='software engineer'/>
   </div>
 
   <div>
